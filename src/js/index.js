fbc8d4f37ea772230f17eb2ae8d10f9039c09846
nadiroayoub <nadiroayoub@gmail.com>
2021-10-08 08:53:03 +0200

    added media queries and designer popup

--- src/js/index.js
+++ src/js/index.js
@@ -10,10 +10,39 @@ const images = importAll(
 );
 
 /**
- * ***************************************** NAVIGATION *******************************************
+ * ***************************************** POPUP *******************************************
  */
+// Variables
+const btnsOpenPopup = document.querySelectorAll(".btn-demo");
 
-// CHANCGE VIEWBOX SIZE
-// const LogoSVG = document.querySelector("#micromerce-logo");
-// LogoSVG.setAttribute("viewBox", "0 0 500 300");
-// console.log(LogoSVG);
+// Add overlow hidden to html document when opening the popup
+btnsOpenPopup.forEach((btnOpenPopup) => {
+  btnOpenPopup.addEventListener("click", () => {
+    document.documentElement.classList.add("no-scroll");
+  });
+});
+
+// close popup when clicked outside of popup
+const popups = document.querySelector(".popup");
+
+window.addEventListener("click", (event) => {
+  var isInsidePopups = popups.contains(event.target);
+  if (window.location.hash == "#overlay" && !isInsidePopups) {
+    window.location.hash = "";
+  }
+});
+
+/**
+ * ***************************************** FAQ SECTION *******************************************
+ */
+// Variables
+const faqQuestions = document.querySelectorAll(".faq_question");
+
+console.log(faqQuestions);
+
+faqQuestions.forEach((faqQuestion, index) => {
+  faqQuestion.addEventListener("click", (e) => {
+    faqQuestion.classList.toggle("active");
+    faqQuestion.nextElementSibling.classList.toggle("active");
+  });
+});
